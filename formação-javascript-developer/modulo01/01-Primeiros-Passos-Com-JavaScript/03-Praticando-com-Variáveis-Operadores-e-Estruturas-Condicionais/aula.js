e5a1643fe6954eca3e8fa668ae254a5103e3281a
Julio <jhmarta7@gmail.com>
2024-11-20 11:32:23 -0300

    add

--- formação-javascript-developer/modulo01/01-Primeiros-Passos-Com-JavaScript/03-Praticando-com-Variáveis-Operadores-e-Estruturas-Condicionais/aula.js
+++ formação-javascript-developer/modulo01/01-Primeiros-Passos-Com-JavaScript/03-Praticando-com-Variáveis-Operadores-e-Estruturas-Condicionais/aula.js
@@ -26,6 +26,9 @@ if (media < 5) {
   console.log("Aprovado");
 }
 
+console.log(
+  "***********************************************************************"
+);
 /*
 O IMC - Indice de Massa Corporal é um criério da Organização Mundial de Saúde para dar 
 uma indicação sobre a condição de peso de uma pessoa adulta.
@@ -62,4 +65,65 @@ if (imc < 18.5) {
   console.log("Obeso");
 } else {
   console.log("Obesidade Grave");
+}
+
+console.log(
+  "***********************************************************************"
+);
+
+/*
+ 3) Elabore um algoritmo que calcule o que deve ser pago por um produto, considerando o preço
+ normal de etiqueta e a escolha da condição de pagamento.
+ Utilize os códigos da tabela a seguir para ler qual a condição de pagamento escolhida e efetuar
+ o cálculo adequado.
+ 
+ Código Condição de pagamento:
+ - À vista Debito, recebe 10% de desconto;
+ - À vista no Dinhero ou Pix, recebe 15% de desconto;
+ - Em duas vezes, preço normal de etiqueta sem juros;
+ - Acima de duas vezes, preço normal de etiqueta mais juros de 10%; 
+ */
+
+const preco = 200.0;
+/*
+Pix e dinheiro = 15% desc;
+Debito = 10%;
+2x = preço normal;
+Acima de 2x = juros 10%.
+*/
+const tipoPagamento = "acima2x";
+
+if (tipoPagamento === "pix" || tipoPagamento === "dinheiro") {
+  const valorAhVista = preco - (preco * 15) / 100; //Juros Simples (15 / 100) = 0.15 * preco = 30.00
+  console.log(
+    `Valor original: $${preco.toFixed(
+      2
+    )} Valor após desconto de 15%: $${valorAhVista.toFixed(2)}`
+  );
+} else if (tipoPagamento === "Debito") {
+  const valorDebito = preco - (preco * 10) / 100; //Juros Simples (10 / 100) = 0.1 * preco = 20.00
+  console.log(
+    `O valor original: $${preco.toFixed(
+      2
+    )} o Valor após desconto de 10%: $${valorDebito.toFixed(2)}`
+  );
+} else if (tipoPagamento === "2x") {
+  const valor2x = preco / 2;
+  console.log(
+    `O valor original: $${preco.toFixed(2)} em 2x
+  Primeira Parcela: $${valor2x.toFixed(2)} Segunda Parcela: $${valor2x.toFixed(
+      2
+    )} `
+  );
+} else if (tipoPagamento === "acima2x") {
+  const valorAhAcima2x = preco + (preco * 10) / 100; //Juros Simples (10 / 100) = 0.1 * preco = 20.00
+  console.log(
+    `O valor original: $${preco.toFixed(
+      2
+    )} Acima de 2x juros de 10%: $${valorAhAcima2x.toFixed(2)}`
+  );
+} else {
+  console.log(
+    "Opção Invalida escolha 'Pix e dinheiro = 15% desc. 'Debito = 10% desc.'  '2x = preço normal' 'Acima de 2x = juros 10%'."
+  );
 }
