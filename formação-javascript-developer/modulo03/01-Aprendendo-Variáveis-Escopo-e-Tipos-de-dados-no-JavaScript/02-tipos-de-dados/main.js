95ec44505629c927efb389f724a17beeff237700
Julio <jhmarta7@gmail.com>
2024-12-06 04:16:10 -0300

    add

--- formação-javascript-developer/modulo03/01-Aprendendo-Variáveis-Escopo-e-Tipos-de-dados-no-JavaScript/02-tipos-de-dados/main.js
+++ formação-javascript-developer/modulo03/01-Aprendendo-Variáveis-Escopo-e-Tipos-de-dados-no-JavaScript/02-tipos-de-dados/main.js
@@ -9,3 +9,24 @@
  */
 
 //object
+
+//true false
+
+const nome = "";
+if (nome) {
+  console.log("preenchido");
+}
+
+// null ausencia de valor
+
+const x = null;
+let y;
+console.log(x);
+console.log(y); //ausencia de declaração
+
+const pessoa = {
+  nome: null,
+};
+
+console.log(pessoa.nome);
+console.log(pessoa.idade);
